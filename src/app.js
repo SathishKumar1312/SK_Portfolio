249e99ae1c5c4ca9f9e74a9221394addac8bb844
SathishKumar1312 <sathishmskcs1312@gmail.com>
2024-11-12 17:50:12 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "./Public")));
+app.use(express.static(path.join(__dirname, "/Public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/Public/index.html"));
